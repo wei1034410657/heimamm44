5a90bad8457248038abdf5ad70752f665dfa3eef
1034410657wei <1034410657@qq.com>
2019-12-13 16:32:11 +0800

    登录模块 - 整体布局

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-//导入接收 路由
-import router from './router/router.js'
 //导入饿了么ui
 import ElementUI from 'element-ui';
 //导入饿了么的样式
 import 'element-ui/lib/theme-chalk/index.css';
+import Vue from 'vue';
+import App from './App.vue';
+//导入接收 路由
+import router from './router/router.js';
 //注册一下
 Vue.use(ElementUI);
+
+//导入全局样式
+import "./style/base.css";
 
 Vue.config.productionTip = false
 
@@ -16,3 +19,4 @@ new Vue({
   //挂载到Vuer示例上 router
   router
 }).$mount('#app')
+
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,13 +1,72 @@
 <template>
-  <div>这是登录页</div>
+  <div class="login-box">
+    <!-- 表单容器 -->
+    <div class="form-box">
+      <!-- 标题 -->
+      <div class="title-box">
+        <img src="../../assets/login-logo.png" alt="">
+        <span class="title">黑马面面</span>
+        <i></i>
+        <span class="sub-title">用户登录</span>
+      </div>
+    </div>
+    <img class="login-pic" src="../../assets/login_banner_ele.png" alt="" />
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  name: "login"
+};
 </script>
 
-<style>
-
+<style lang="less">
+.login-box {
+  /* 开启弹性布局 */
+  display: flex;
+  /* 上下居中 */
+  align-items: center;
+  /* 左右 均分 */
+  justify-content: space-around;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  height: 100%;
+  .form-box {
+    width: 478px;
+    height: 550px;
+    background-color: #f5f5f5;
+    padding: 44px;
+    box-sizing: border-box;
+    // 标题
+    .title-box{
+      display: flex;
+      align-items: center;
+      img{
+        // emmet语法
+        // w22+h17+mr16
+        width: 22px;
+        height: 17px;
+        margin-right: 16px;
+      }
+      .title{
+        font-size: 24px;
+        margin-right: 14px;
+      }
+      i{
+        width: 1px;
+        height: 28px;
+        background-color: #C7C7C7;
+        margin-right: 12px;
+      }
+      .sub-title{
+        font-size: 22px;
+      }
+    }
+  }
+  .login-pic {
+  }
+}
 </style>
